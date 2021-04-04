9373466816a1c841184a58791f0b5b000645cc2a
guilherj <contato.guilhermenascimento@hotmail.com>
2021-04-04 10:15:09 -0300

    1 Versão - Com os comentários em application.Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,6 +17,11 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
+		// ORDENANDO A LISTA COM O COLLECTIONS.SORT
+		// PORÉM CASO O CRITERIO DE COMPARAÇÃO MUDE É 
+		// NECESSÁRIO MEXER DENTRO DA CLASSE PRODUCT, ISSO EM TERMOS DE
+		// MANUTENÇÕES É INDESEJÁVEL.
+		
 		Collections.sort(list);
 		
 		for(Product p : list) {
