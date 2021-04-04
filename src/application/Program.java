a16b237304bd49070e1ebd31ab8a6fbbed3ad97b
guilherj <contato.guilhermenascimento@hotmail.com>
2021-04-04 11:06:38 -0300

    Versão 4 - Expressões lambda: Criando uma função anônima

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,38 +7,26 @@ import java.util.List;
 import entities.Product;
 
 public class Program {
-	
+
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-		
-		/* CRIANDO UMA SINTAXE DE CLASSE ANÔNIMA DENTRO DE PROGRAM 
-		 * PARA ORGANIZAR A LIST DE PRODUCT SEM CRIAR UMA CLASSE QUE IMPLEMENTA
-		 * A INTERFACE COMPARATOR
-		 */
-		Comparator <Product> comp = new Comparator<Product>() {
 
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
-			
+		// COMEÇANDO A USAR AS EXPRESSÕES LAMNDAS CRIANDO UMA FUNÇÃO ANÔNIMA.
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
-		
-		// CHAMANDO O OBJETO comp QUE É UMA INSTANCIA DA CLASSE ANÔNIMA E QUE TEM A FUNÇÃO 
-		// COMPARE IMPLEMENTADA.
+
 		list.sort(comp);
 
-				
-		for(Product p : list) {
+		for (Product p : list) {
 			System.out.println(p);
 		}
-		
+
 	}
 
 }
